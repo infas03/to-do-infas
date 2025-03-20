a09e28196534049da0935a2746e11282d2dabfc6
infas03 <infasmax03@gmail.com>
2025-03-20 23:59:50 +0530

    update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,27 +5,33 @@ import { VersioningType } from '@nestjs/common';
 
 dotenv.config();
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   });
+
+//   app.setGlobalPrefix('api');
+
+//   app.enableVersioning({
+//     type: VersioningType.URI,
+//   });
+
+//   const port = process.env.PORT || 3000;
+//   await app.listen(port);
+
+//   console.log(`Server is running on http://localhost:${port}`);
+// }
+
+// bootstrap().catch((err) => {
+//   console.error('Error during bootstrap:', err);
+// });
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-  app.setGlobalPrefix('api');
-
-  app.enableVersioning({
-    type: VersioningType.URI,
-  });
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-
-  console.log(`Server is running on http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-bootstrap().catch((err) => {
-  console.error('Error during bootstrap:', err);
-});
+bootstrap();
